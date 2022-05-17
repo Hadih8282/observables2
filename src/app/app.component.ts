e304e9a5f339d40e044305075f3997f7977e00fb
Hadih8282 <hadi_h82@gmail.com>
2022-05-17 16:02:33 +0430

    observabes3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable, shareReplay } from 'rxjs';
+import { Todo } from './models/models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'observables2';
+
+  public http$!: Observable<Todo[]>;
+  public doneTask$!: Observable<Todo[]>;
+  public undoneTask$!: Observable<Todo[]>;
+
+
+
+
+  public ngOnInit(): void {
+    
+    this.http$ = new Observable((observer) => {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((response) => {
+          // console.log(response); 
+          // console.log(response.json());
+          return response.json();
+        })
+        .then((body) => {
+          // console.log(body);
+          observer.next(body);
+          observer.complete();
+        })
+        .catch((err) => {
+          // console.log(err);
+          observer.error(err);
+        });
+    });
+    
+
+    this.http$ = this.http$.pipe(
+      shareReplay()
+    );
+
+
+    this.doneTask$ =this.http$.pipe(
+      map((todo) => todo.filter((todo) => todo.completed === true))
+    );
+
+    this.undoneTask$ =this.http$.pipe(
+      map((todo) => todo.filter((todo) => todo.completed === false))
+    );
+
+  }
+
+
 }
